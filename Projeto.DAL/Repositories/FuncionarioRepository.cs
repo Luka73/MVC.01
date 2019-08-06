482e0bfcc016e96e62ac4fdc1c49d0f2dbf97835
Luka73 <luanafernandes73@gmail.com>
2019-08-06 10:52:14 -0300

    Finishing DAL

--- Projeto.DAL/Repositories/FuncionarioRepository.cs
+++ Projeto.DAL/Repositories/FuncionarioRepository.cs
@@ -22,6 +22,7 @@ namespace Projeto.DAL.Repositories
             string query = "insert into Funcionario(Nome, Salario, DataAdmissao) values (@Nome, @Salario, @DataAdmissao)";
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
+                connection.Open();
                 SqlCommand command = new SqlCommand(query, connection);
                 command.Parameters.AddWithValue("@Nome", obj.Nome);
                 command.Parameters.AddWithValue("@Salario", obj.Salario);
@@ -34,6 +35,7 @@ namespace Projeto.DAL.Repositories
             string query = "update Funcionario set Nome=@Nome, Salario=@Salario, DataAdmissao=@DataAdmissao where IdFuncionario=@IdFuncionario";
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
+                connection.Open();
                 SqlCommand command = new SqlCommand(query, connection);
                 command.Parameters.AddWithValue("@IdFuncionario", obj.IdFuncionario);
                 command.Parameters.AddWithValue("@Nome", obj.Nome);
@@ -43,20 +45,83 @@ namespace Projeto.DAL.Repositories
             }
         }
         public void Delete(int id)
-        {
+        { 
             string query = "delete from Funcionario where IdFuncionario=@IdFuncionario";
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@IdFuncionario", id);
+                command.ExecuteNonQuery();
+            }
         }
         public Funcionario SelectById(int id)
-        {
+        { 
             string query = "select * from Funcionario where IdFuncionario=@IdFuncionario";
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@IdFuncionario", id);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if(reader.Read())
+                {
+                    Funcionario funcionario = new Funcionario();
+                    funcionario.IdFuncionario = Convert.ToInt32(reader["IdFuncionario"]);
+                    funcionario.Nome = Convert.ToString(reader["Nome"]);
+                    funcionario.Salario = Convert.ToDecimal(reader["Salario"]);
+                    funcionario.DataAdmissao = Convert.ToDateTime(reader["DataAdmissao"]);
+                    return funcionario;
+                }
+                return null;
+            }
         }
         public List<Funcionario> SelectAll()
         {
             string query = "select * from Funcionario";
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                List<Funcionario> lista = new List<Funcionario>();
+
+                while (reader.Read())
+                {
+                    Funcionario funcionario = new Funcionario();
+                    funcionario.IdFuncionario = Convert.ToInt32(reader["IdFuncionario"]);
+                    funcionario.Nome = Convert.ToString(reader["Nome"]);
+                    funcionario.Salario = Convert.ToDecimal(reader["Salario"]);
+                    funcionario.DataAdmissao = Convert.ToDateTime(reader["DataAdmissao"]);
+                    lista.Add(funcionario);
+                }
+                return lista;
+            }
         }
         public List<Funcionario> SelectAll(string nome)
         {
             string query = "select * from Funcionario where Nome like @Nome";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(connectionString);
+                command.Parameters.AddWithValue("@Nome", string.Format("{0}%", nome));
+                SqlDataReader reader = command.ExecuteReader();
+
+                List<Funcionario> lista = new List<Funcionario>();
+
+                while (reader.Read())
+                {
+                    Funcionario funcionario = new Funcionario();
+                    funcionario.IdFuncionario = Convert.ToInt32(reader["IdFuncionario"]);
+                    funcionario.Nome = Convert.ToString(reader["Nome"]);
+                    funcionario.Salario = Convert.ToDecimal(reader["Salario"]);
+                    funcionario.DataAdmissao = Convert.ToDateTime(reader["DataAdmissao"]);
+                    lista.Add(funcionario);
+                }
+                return lista;
+            }
         }
     }
 }
