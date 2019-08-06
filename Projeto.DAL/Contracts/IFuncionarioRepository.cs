f6fc16a5e95ccf8aab1c4cbadb1af19d39d4da0b
Luka73 <luanafernandes73@gmail.com>
2019-08-06 10:28:23 -0300

    Add DAL

---
+++ Projeto.DAL/Contracts/IFuncionarioRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto.DAL.Entities;
+
+namespace Projeto.DAL.Contracts
+{
+    public interface IFuncionarioRepository
+        : IBaseRepository<Funcionario>
+    {
+        List<Funcionario> SelectAll(string nome);
+    }
+}
