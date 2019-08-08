c73d9d4d361f97a675a8e48840c0d457a8de1f23
Luka73 <luanafernandes73@gmail.com>
2019-08-08 08:41:00 -0300

    Including view for sign up

---
+++ Projeto.Presentation/Controllers/FuncionarioController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Projeto.Presentation.Models;
+using Projeto.DAL.Entities;
+using Projeto.DAL.Repositories;
+
+namespace Projeto.Presentation.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        // GET: Funcionario/Cadastro
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        //POST: Funcionario/Cadastro
+        [HttpPost] //Receber o submit do formulario
+        public ActionResult Cadastro(FuncionarioCadastroModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Funcionario funcionario = new Funcionario();
+                    funcionario.Nome = model.Nome;
+                    funcionario.Salario = model.Salario;
+                    funcionario.DataAdmissao = model.DataAdmissao;
+
+                    FuncionarioRepository repository = new FuncionarioRepository();
+                    repository.Insert(funcionario);
+
+                    TempData["Mensagem"] = "Funcionário cadastrado com sucesso!";
+                    ModelState.Clear();
+                }
+                catch(Exception e)
+                {
+                    TempData["Mensagem"] = e.Message;
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Projeto.Presentation/Models/FuncionarioCadastroModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projeto.Presentation.Models
+{
+    public class FuncionarioCadastroModel
+    {
+        [MinLength(3, ErrorMessage = "Por favor, informe no mínimo {1} caracteres.")]
+        [MaxLength(50, ErrorMessage = "Por favor, informe no máximo {1} caracteres.")]
+        [Required(ErrorMessage = "Por favor, preencha o nome do Funcionário.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Por favor, preencha o salário do Funcionário.")]
+        [Range(100.00, 20000.00, ErrorMessage = "O salário deve estar entre {1} e {2}.")]
+        public decimal Salario { get; set; }
+
+        [Required(ErrorMessage = "Por favor, preencha a data de admissão do Funcionário.")]
+        public DateTime DataAdmissao { get; set; }
+    }
+}
